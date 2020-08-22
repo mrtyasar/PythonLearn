d64f09aed4781f9d3adc9b805c0e40c94bff66ac
mrtyasar <rulilero12@gmail.com>
2020-08-23 02:55:45 +0300

    Create ComparisonOperators.py

---
+++ with mosh everybody python/ComparisonOperators.py
@@ -0,0 +1,24 @@
+temperature = 35
+
+if temperature > 30:
+    print("ıt's a hot day")
+else:
+    print("It's not a hot day")
+    
+
+name = "j"
+if len(name) < 3:
+    print("name değişkeni 3 karakter az bir karakter dizisini barındırıyor.")
+elif len(name) > 50:
+    print("name değişkeni 50 karakterden fazla bir karakter dizisi barınıdırıyor.")
+else:
+    print('isim güzel gözüküyor!')
+
+# = atama işareti
+# == eşitti işareti
+# <= küçük veya eşittir
+# >= büyük veya eşittir
+# < küçüktür
+# > büyüktür
+# != eşit değildir
+
