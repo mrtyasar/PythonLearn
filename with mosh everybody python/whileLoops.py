7e9f23bead120b7c061e15a245bd94dd94d0d74b
mrtyasar <rulilero12@gmail.com>
2020-08-24 22:14:18 +0300

    Create whileLoops.py

---
+++ with mosh everybody python/whileLoops.py
@@ -0,0 +1,13 @@
+i = 1
+while i <=5:
+    print(i)
+    i += 1
+print('Done!')
+
+# farklı bir şekilde çıktı alalım
+print('-------------------')
+i = 1
+while i <= 5:
+    print('*'*i)
+    i += 1
+print('Done!')
