c966fefe0a0a199d3f0eecb3c9a1b99d4fbaa04a
MuratCan <59768661+mrtyasar@users.noreply.github.com>
2020-08-27 22:24:12 +0300

    Create unpacking.py

---
+++ with mosh everybody python/unpacking.py
@@ -0,0 +1,19 @@
+coordinates = (1,2,3)
+# bu değişkendeki ögeleri tek tek kullanalım
+
+# kullanacağımız yerde ögeleri indeks numaralarına göre çağırabiliriz
+coordinates[0],coordinates[1],coordinates[2]
+print(coordinates[0])
+
+# veya ögeleri değişkenlere atayabiliriz
+x = coordinates[0]
+y = coordinates[1]
+z = coordinates[2]
+print(y)
+
+# veya ve en kullanışlı yöntem ise toplu olarak değişkenlere atamaktır
+x1,y1,z1 = coordinates[0],coordinates[1],coordinates[2]
+print(z1)
+
+# bu şekil toplu olarak ögeleri veya değerleri değişkenlere atayabiliriz
+
