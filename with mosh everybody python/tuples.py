01c92773dd32dcf730d097988f07c7c2330eeb04
MuratCan <59768661+mrtyasar@users.noreply.github.com>
2020-08-27 22:12:38 +0300

    Create tuples.py

---
+++ with mosh everybody python/tuples.py
@@ -0,0 +1,12 @@
+# tuple listeler gibi birden fazla ögeyi içinde saklayabilir
+# fakat listeler gibi tuple daki ögeleri değiştiremeyiz, ekleyemeyiz, kaldıramayız
+# tuple () içinde oluşturulur
+numbers = (1,2,3)
+# tuple da ögelere erişim
+print(numbers[0])
+
+# eğer tuple nın üzerinde değişiklik yapmaya çalışırsak TypeError hatası alırız
+
+# liste yerine tuple kullanmamızdaki amaç eğer ögelerin üzerinde bir değişiklik yapmayacaksak
+# veya yapılmasını istemiyorsak tuple kullanmak çok daha iyi olur
+
