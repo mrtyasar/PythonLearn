52cc844207adec624c86891140ac11258d95ed57
MuratCan <59768661+mrtyasar@users.noreply.github.com>
2020-08-28 23:23:59 +0300

    Create comments.py

---
+++ with mosh everybody python/comments.py
@@ -0,0 +1,13 @@
+# yorumları sık sık kod yazarken kullanacağız
+# bunun nedeni hem daha sonraları kodumuzda neler yapmışız onları hatırlamak
+# hem de başkası kodlarda neler yaptığımızı daha iyi anlayasın diye kullanabiliriz
+
+# yorumlar çıktılarda gözükmez
+print('Hayat bence güzel')
+
+# yorum yazarken kodu açıklayan, nedenlik içeren açıklamalar kullanılmaz
+# gereksiz yere yazılan kodlar kod okunurluğunu düşürebilir
+
+
+
+ 
