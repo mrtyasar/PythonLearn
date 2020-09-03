7f21bd18c908bda8b13e94c28f4653987fbc7f5d
MuratCan <59768661+mrtyasar@users.noreply.github.com>
2020-09-03 03:55:30 +0300

    Create pypiAndPip.py

---
+++ with mosh everybody python/pypiAndPip.py
@@ -0,0 +1,14 @@
+# pypi.org' a bak
+
+# pypi.org pythonda kullanılabilen paketleri toplayan bir sitedir
+# örneğin sms uygulaması için buradan paket kurabiliriz
+# tabii kide hepsi bugsuz değil bunlar diğer kullanıcılar tarafından geliştirilir
+
+# paket kurmak indirmek:
+
+# zaten sitede kurmak için komut verecek
+# çoğunlukla pip install ... şeklinde olur
+
+# pip install openpyxl komutunu termianlde çalıştıralarak
+# openpyxl paketini kurabildik bu sayede excel dosyaların üzerinde işlem yapabiliriz
+
